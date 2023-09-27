54f7381d8a9e501326f5a57b075f91d90a65e182
1Matzh <matheusbenasse@gmail.com>
2023-09-27 17:56:17 -0300

    correção geral + texto (test)

--- js/app.js
+++ js/app.js
@@ -1,12 +1,60 @@
-gsap.registerPlugin(ScrollTrigger);
+const canvas = document.querySelector(".canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+document.documentElement.style.overflowX = 'hidden';
 
-const demoVideo1Info = {
-  totalFrames: 158,
-  totalTime: 15,
-  images: [],
-  currentFrame: 0,
-  currentImage: (index) =>
-    `./img/spaceship-frames/spaceship_anim_${index.toString().padStart(3, "0")}.jpg`,
-};
+const context = canvas.getContext("2d");
+const frameCount = 158;
 
-animateOnScroll("video_id", demoVideo1Info);
+const currentFrame = (index) => `./img/spaceship-frames/spaceship_anim_${index.toString().padStart(3, "0")}.jpg`;
+
+const images = [];
+let ball = { frame: 0 };
+
+for (let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.src = currentFrame(i);
+  console.log(currentFrame(i));
+  images.push(img);
+}
+
+gsap.to(ball, {
+  frame: frameCount - 1,
+  snap: "frame",
+  ease: "none",
+  scrollTrigger: {
+    scrub: 0.5,
+    pin: "canvas",
+    end: "500%",
+  },
+  onUpdate: render,
+});
+
+gsap.fromTo(
+  ".anim-text",
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    scrollTrigger: {
+      scrub: 1,
+
+      start: "20%",
+      end: "50%",
+    },
+    onComplete: () => {
+      gsap.to(".anim-text", { opacity: 0 });
+    },
+  }
+);
+
+images[0].onload = render;
+
+function render() {
+  context.canvas.width = images[0].width;
+  context.canvas.height = images[0].height;
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(images[ball.frame], 0, 0);
+}
